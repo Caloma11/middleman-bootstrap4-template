e5cfed987bf55fe0bf9e143797b2f547979e8680
Benoit Calin <benoit.calin@gmail.com>
2019-04-04 12:01:30 +0200

    Adapt the template in order to have it working with bootstrap 4

--- config.rb
+++ config.rb
@@ -4,13 +4,17 @@ end
 
 activate :sprockets
 
+# ------ Uncomment and adapt if you want to use i18n ------
+# activate :i18n, mount_at_root: :fr, langs: [:fr, :en]
+
 page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
 
 configure :build do
   activate :minify_css
-  activate :minify_javascript
+  # ------ We commented this command as it prevented us to deploy our Middleman project on GitHub Pages ------
+  # activate :minify_javascript
   activate :asset_hash
   activate :relative_assets
   set :relative_links, true
